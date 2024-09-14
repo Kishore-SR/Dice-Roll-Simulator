744f4317bc8bb29672cf70fea72ea90dc2c7287e
Kishore S R <kishoresr01@gmail.com>
2024-09-13 23:47:49 -0400

    Added JS code

---
+++ script.js
@@ -0,0 +1,55 @@
+const buttonEl = document.getElementById("roll-button");
+const diceEl = document.getElementById("dice");
+const rollHistoryEl = document.getElementById("roll-history");
+
+// Array to store the history of dice roll
+let historyList = [];
+
+// Function to randomly generate a number b/w 1 to 6
+function rollDice() {
+  const rollResult = Math.floor(Math.random() * 6) + 1;
+  const diceFace = getDiceFace(rollResult);
+  diceEl.innerHTML = diceFace;
+  historyList.push(rollResult);
+  updateRollHistory();
+}
+
+function updateRollHistory() {
+  rollHistoryEl.innerHTML = "";
+  for (let i = 0; i < historyList.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(
+      historyList[i]
+    )}</span>`;
+    rollHistoryEl.appendChild(listItem);
+  }
+}
+
+// Function to update the 'Dice Face' on the screen based on the rolled results
+function getDiceFace(rollResult) {
+  switch (rollResult) {
+    case 1:
+      return "&#9856;";
+    case 2:
+      return "&#9857;";
+    case 3:
+      return "&#9858;";
+    case 4:
+      return "&#9859;";
+    case 5:
+      return "&#9860;";
+    case 6:
+      return "&#9861;";
+    default:
+      return "";
+  }
+}
+
+// Function for Dice rolling simple animation (using css)
+buttonEl.addEventListener("click", () => {
+  diceEl.classList.add("roll-animation");
+  setTimeout(() => {
+    diceEl.classList.remove("roll-animation");
+    rollDice();
+  }, 1000);
+});
